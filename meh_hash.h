49bb5ff6a94bc93d8778a04fa47f193c07bc2bb2
Michał Gorzka <michal2002xx@gmail.com>
2024-10-10 16:09:36 +0200

    Table simple abstractions, cleanup.

--- meh_hash.h
+++ meh_hash.h
@@ -11,9 +11,11 @@
 namespace meh {
     inline uint64_t next_pow_2(uint64_t num);
 
-    template <typename TKey, typename TValue, uint64_t (*func_hash)(const TKey &), bool (*func_compare)(const TKey &, const TKey &)>
+    template <typename TKey, typename TValue, uint64_t (*func_hash)(const TKey &) = NULL, bool (*func_compare)(const TKey &, const TKey &) = NULL>
     class Table {
-        static constexpr uint32_t LOAD_FACTOR = 70;
+
+        // @todo Make a template parameter
+        static constexpr uint32_t LOAD_FACTOR = 68;
 
         /* Table data structure */
         struct Bucket {
@@ -21,34 +23,94 @@ namespace meh {
             TKey   key;
             TValue value;
         };
-        
+
         /* Calculate hash and mod to fit in the bucket array */
         uint64_t calc_hash(const TKey &key) const {
-            uint64_t hash = func_hash(key) % (m_buckets_allocated - 17); // @todo
+            uint64_t hash = func_hash(key) % (m_buckets_allocated - 1); // @todo can i -17 here?
             return hash;
         }
 
         /* Check if the table should be expanded due to load factor */
         bool should_expand_table(void) {
+#if 0
             double occupied_perc = (double)(m_buckets_occupied + 1) / (double)m_buckets_allocated;
             return (occupied_perc * 100.0) >= LOAD_FACTOR;
+#else
+            return m_buckets_occupied * 100 >= m_buckets_allocated * LOAD_FACTOR;
+#endif
         }
 
-        /* Rehash values from old table data */
-        void rehash_table(Bucket *buckets, uint64_t num_of_buckets) {
-            for(uint64_t bucket_index = 0; bucket_index < num_of_buckets; ++bucket_index) {
-                Bucket *bucket = buckets + bucket_index;
+        /* Reallocate memory and rehash values from old table data */
+        void rehash_table(uint64_t buckets_to_allocate) {
+            assert(buckets_to_allocate >= m_buckets_allocated);
+
+            Bucket  *old_buckets = m_buckets;
+            uint64_t old_buckets_allocated = m_buckets_allocated;
+            uint64_t new_size_in_bytes = buckets_to_allocate * sizeof(Bucket);
+
+            /* Clear newly allocated table */
+            m_buckets_occupied = 0;
+            m_buckets_allocated = buckets_to_allocate;
+            m_buckets = (Bucket *)malloc(new_size_in_bytes);
+            memset(m_buckets, 0, new_size_in_bytes);
+
+            for(uint64_t bucket_index = 0; bucket_index < old_buckets_allocated; ++bucket_index) {
+                Bucket *bucket = old_buckets + bucket_index;
 
                 if(bucket->is_occupied) {
                     this->insert(bucket->key, bucket->value);
                 }
             }
+
+            free(old_buckets);
+        }
+
+        TValue *find_by_key(const TKey &key) {
+            uint64_t hash = this->calc_hash(key);
+            uint64_t hash_start = hash;
+
+            _find_collisions = 0;
+
+            while(true) {
+                if(!m_buckets[hash].is_occupied) {
+                    return NULL;
+                }
+
+                if(func_compare(key, m_buckets[hash].key)) {
+                    return &m_buckets[hash].value;
+                }
+
+                hash += 1;
+
+                _find_collisions += 1;
+
+                /* Wrap */
+                if(hash >= m_buckets_allocated) {
+                    hash = 0;
+                }
+
+                /* Went through whole table and did not find key */
+                if(hash == hash_start) {
+                    return NULL;
+                }
+            }
+
+            return NULL;
         }
 
 public:
+        /* When iterate_all returns false, the *key* and *value* may contain garbage value */
+        struct Iterator {
+            uint64_t iter_id = 0;
+            TKey     key;
+            TValue  *value;
+        };
+
         /* Intial size rounds to next power of two */
         Table(uint64_t init_size = 128) {
             init_size = next_pow_2(init_size);
+
+            /* Clamp size to be atleast 128 buckets */
             init_size = init_size < 128 ? 128 : init_size;
 
             uint64_t size_in_bytes = init_size * sizeof(Bucket);
@@ -109,90 +171,27 @@ public:
             return &bucket.value;
         }
 
-        /* Get value at key */
-        TValue *const find(const TKey &key) {
-            uint64_t hash = this->calc_hash(key);
-            uint64_t hash_start = hash;
-
-            _find_collisions = 0;
-
-            while(true) {
-                if(!m_buckets[hash].is_occupied) {
-                    return NULL;
-                }
-
-                if(func_compare(key, m_buckets[hash].key)) {
-                    return &m_buckets[hash].value;
-                }
-
-                hash += 1;
-
-                _find_collisions += 1;
-
-                /* Wrap */
-                if(hash >= m_buckets_allocated) {
-                    hash = 0;
-                }
-
-                /* Went through whole table and did not find key */
-                if(hash == hash_start) {
-                    return NULL;
-                }
-            }
-
-            return NULL;
+        /* Get pointer to the value at key */
+        TValue *find(const TKey &key) {
+            TValue *value = this->find_by_key(key);
+            return value;
         }
 
         const TValue *const find(const TKey &key) const {
-            uint64_t hash = this->calc_hash(key);
-            uint64_t hash_start = hash;
-
-            while(true) {
-                if(!m_buckets[hash].is_occupied) {
-                    return NULL;
-                }
-
-                if(func_compare(key, m_buckets[hash].key)) {
-                    return &m_buckets[hash].value;
-                }
-
-                hash += 1;
-
-                /* Wrap */
-                if(hash >= m_buckets_allocated) {
-                    hash = 0;
-                }
-
-                /* Went through whole table and did not find key */
-                if(hash == hash_start) {
-                    return NULL;
-                }
-            }
-
-            return NULL;
+            const TValue *const value = this->find_by_key(key);
+            return value;
         }
 
         /* Resize table to the next power of 2 and rehash entries */
         void expand_table(void) {
-            Bucket  *old_buckets = m_buckets;
-            uint64_t old_buckets_allocated = m_buckets_allocated;
-
-            uint64_t buckets_to_allocate = next_pow_2(old_buckets_allocated + 1);
-            uint64_t size_in_bytes = buckets_to_allocate * sizeof(Bucket);
-
-            m_buckets_allocated = buckets_to_allocate;
-            m_buckets = (Bucket *)malloc(size_in_bytes);
-            m_buckets_occupied = 0;
-            memset(m_buckets, 0, size_in_bytes);
-
-            this->rehash_table(old_buckets, old_buckets_allocated);
-
-            free(old_buckets);
+            uint64_t new_number_of_buckets = next_pow_2(m_buckets_allocated + 1);
+            fprintf(stdout, "EXPANDING %llu -> %llu (perc: %f)\n", m_buckets_allocated, new_number_of_buckets, (double)m_buckets_occupied / (double)m_buckets_allocated);
+            this->rehash_table(new_number_of_buckets);
         }
 
         /* Rehashes the table with the same size @todo, when added removing elements */
         void rehash_table(void) {
-            assert(NULL);
+            this->rehash_table(m_buckets_allocated);
         }
 
         /* Clears the table but does not reallocate memory */
@@ -219,13 +218,6 @@ public:
             return m_buckets_allocated;
         }
     
-        /* When iterate_all returns false, the *key* and *value* may contain garbage value */
-        struct Iterator {
-            uint64_t iter_id = 0;
-            TKey     key;
-            TValue  *value;
-        };
-
         /* *iter_id* must start at 0 and must not be modified */
         bool iterate_all(Iterator &iter) {
             while(true) {
@@ -246,7 +238,7 @@ public:
             return false;
         }
 
-        /* Calls the macro on every found bucket */
+        /* Calls *func* on every found bucket */
         void call_on_every(void (*func)(TKey key, TValue *value)) {
             Iterator iter;
             while(this->iterate_all(iter)) {
@@ -255,7 +247,7 @@ public:
         }
 
         uint32_t _insert_collisions = 0;
-        uint32_t _find_collisions   = 0; // Reseted on start of find() function
+        uint32_t _find_collisions   = 0; // Reset on start of find() function
 private:
         uint64_t  m_buckets_allocated;
         uint64_t  m_buckets_occupied;
@@ -270,6 +262,54 @@ private:
         }
         return res;
     }
+
+    /* @todo Can figure hash function based on template type in compile time??? */
+
+    inline uint64_t _func_hash_def_int32_t(const int32_t &key) {
+        int32_t _key = key * 31;
+        return *(uint64_t *)&_key; 
+    }
+
+    inline bool _func_compare_def_int32_t(const int32_t &key_a, const int32_t &key_b) {
+        return key_a == key_b;
+    }
+
+    inline uint64_t _func_hash_def_int64_t(const int64_t &key) {
+        int64_t _key = key * 31;
+        return *(uint64_t *)&_key; 
+    }
+
+    inline bool _func_compare_def_int64_t(const int64_t &key_a, const int64_t &key_b) {
+        return key_a == key_b;
+    }
+
+    typedef Table<int32_t, int32_t, _func_hash_def_int32_t, _func_compare_def_int32_t> TableInt32;
+    typedef Table<int64_t, int64_t, _func_hash_def_int64_t, _func_compare_def_int64_t> TableInt64;
+
+    template<typename TKey>
+    inline uint64_t _func_hash_def_simple_32_or_64(const TKey &key) {
+        // Is this compiletime???
+        if(sizeof(TKey) == 4) {
+            uint32_t _key32 = *(uint32_t *)&key;
+            uint64_t _key64 = _key32 * 31;
+            return _key64;
+        } else if(sizeof(TKey) == 8) {
+            uint64_t _key = *(uint64_t *)&key;
+            _key *= 31;
+            return _key; 
+        } else {
+            assert(NULL);
+        }
+    }
+
+    template<typename TKey>
+    inline bool _func_compare_def_simple_32_or_64(const TKey &key_a, const TKey &key_b) {
+        assert(sizeof(TKey) == 4 || sizeof(TKey) == 8);
+        return key_a == key_b;
+    }
+
+    template <typename TKey, typename TValue>
+    using TableSimple = Table<TKey, TValue, _func_hash_def_simple_32_or_64, _func_compare_def_simple_32_or_64>;
 }
 
 #endif /* __MEH_HASH */
--- meh_hash_test.cpp
+++ meh_hash_test.cpp
@@ -14,6 +14,49 @@ int32_t pow(int32_t b, int32_t p) {
 
 int main(int argc, char *argv[]) {
     srand(time(NULL));
-    
+ 
+    {
+        meh::TableInt64 table;
+
+        for(int64_t i = 0; i < 8000000; ++i) {
+            table.insert((int64_t)(rand() % RAND_MAX) | ((int64_t)(rand() % RAND_MAX) << 16) | ((int64_t)(rand() % RAND_MAX) << 32) | ((int64_t)(rand() % RAND_MAX) << 48), rand());
+        }
+
+        table.rehash_table();
+
+        fprintf(stdout, "size now: %llu\ninserted: %llu\ncollisions: %u\ncol/insert: %f\n\n", table.get_size(), table.get_count(), table._insert_collisions, (double)table._insert_collisions / (double)table.get_count());
+    }
+
+    {
+        meh::TableInt32 table;
+
+        for(int32_t i = 0; i < 8000000; ++i) {
+            table.insert((int32_t)(rand() % RAND_MAX) | ((int32_t)(rand() % RAND_MAX) << 16), rand());
+        }
+
+        table.rehash_table();
+
+        fprintf(stdout, "size now: %llu\ninserted: %llu\ncollisions: %u\ncol/insert: %f\n\n", table.get_size(), table.get_count(), table._insert_collisions, (double)table._insert_collisions / (double)table.get_count());
+    }
+
+    {
+        meh::TableSimple<uint32_t, int *> table;
+
+        for(int32_t i = 0; i < 8000000; ++i) {
+            table.insert((int32_t)(rand() % RAND_MAX) | ((int32_t)(rand() % RAND_MAX) << 16), (int *)rand());
+        }
+
+        table.rehash_table();
+
+        fprintf(stdout, "size now: %llu\ninserted: %llu\ncollisions: %u\ncol/insert: %f\n\n", table.get_size(), table.get_count(), table._insert_collisions, (double)table._insert_collisions / (double)table.get_count());
+
+        meh::TableSimple<uint32_t, int *>::Iterator iter;
+        while(table.iterate_all(iter)) {
+            // fprintf(stdout, "Found %p at %u !\n", *iter.value, iter.key);
+        }
+    }
+
+
+
     return 0;
 }
